01019ad4f0fa985c37342b08505ca9ea6b2d3e19
Diego Viola <diego.viola@gmail.com>
2013-12-28 00:21:29 -0200

    add create method for classrooms

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -2,4 +2,17 @@ class ClassroomsController < ApplicationController
   def new
     @classroom = Classroom.new
   end
+
+  def create
+    @classroom = Classroom.new(classroom_params)
+    if @classroom.save
+      flash[:notice] = "Classroom saved"
+      redirect_to new_classroom_path
+    end
+  end
+
+  private
+  def classroom_params
+    params.require(:classroom).permit(:level, :sublevel)
+  end
 end
