06dbdc8bb44c2d48621245730de72861a1a8309a
Diego Viola <diego.viola@gmail.com>
2014-04-27 06:52:13 -0300

    add routes back

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "home/index"
+  resources :classrooms, only: [:new, :create]
+  resources :teachers, only: [:new, :create]
+  resources :students, only: [:new, :create]
+
+  root "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
